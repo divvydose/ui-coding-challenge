962bd8144d8f27dc79b1ca5fc8e8ea66b636054d
Jonathan Rubins <jonrubins@gmail.com>
2020-10-26 10:01:56 -0500

    Add support for displaying cats

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import './App.scss'
 
 const App = () => {
-  return <div className="app">This is the application.</div>
+  return <div className="app">This is the application. Cats are cool too.</div>
 }
 
 export default hot(App)
